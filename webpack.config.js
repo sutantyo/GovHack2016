33972c63a99aec825ec5077e6307657a97660dd7
Daniel Sutantyo <daniels@Daniels-MacBook-Air.local>
2016-07-29 16:53:14 +1000

    skeleton files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	entry: "./private/js/entry.jsx",
+	output: {
+		path: __dirname + "/public/js",
+		filename: "bundle.js" // default output filename
+	},
+	resolve: {
+		extensions: ['','.js','.jsx']
+	},
+  module: {
+		loaders: [
+			{ test: /\.js[x]?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				query: { presets: ['react','es2015'] }
+			}
+		]//end loaders
+	}
+};
